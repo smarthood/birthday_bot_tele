00233268e2b0e39db957e924f003e86a9fc02491
smarthood <renolddickson18@gmail.com>
2023-02-08 12:22:16 +0530

    new deploy

---
+++ src/index.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+const {Telegraf} = require('telegraf')
+const bot = new Telegraf(process.env.token)
+const birthdays = require('./birthdays.json')
+const holidays = require('./holidays.json')
+
+bot.start((ctx) => {
+  const chatId = ctx.chat.id
+  const userName = ctx.chat.last_name
+  ctx.reply(`Welcome ${userName}✨`)
+  ctx.reply(`Type /list to see your friends birthday!!`)
+})
+
+bot.command('/time', (ctx) => {
+  const chatId = ctx.chat.id
+  const today = new Date().toISOString()
+  ctx.reply(today)
+})
+
+bot.hears([/Hi|Hello|hi|hello|hey|Hii/], (ctx) => {
+  ctx.reply('Hi ✋ ')
+})
+
+bot.command('/list', (ctx) => {
+  const chatId = ctx.chat.id
+  birthdays.forEach((item) => {
+    ctx.reply(`${item.name} | ${item.date}`)
+  })
+})
+
+const getUpcomingBirthdays = () => {
+  const today = new Date().toISOString()
+  let currentDate = today.substring(5, 10)
+  const upcomingBirthdays = birthdays.filter((birthday) => {
+    const bd = birthday.date.substring(5, 10)
+    return bd === currentDate
+  })
+  return upcomingBirthdays
+}
+
+const getComingFeast = () => {
+  const today = new Date().toISOString()
+  let currentDate = today.substring(5, 10)
+  const comingFeast = holidays.filter((holiday) => {
+    const bd = holiday.date.substring(5, 10)
+    return bd === currentDate
+  })
+  return comingFeast
+}
+
+setInterval(() => {
+  const upcomingBirthdays = getUpcomingBirthdays()
+  const comingFeast = getComingFeast()
+  comingFeast.forEach((holiday) => {
+    bot.telegram.sendMessage(-1001434326296, `Happy ${holiday.name}`)
+  })
+  upcomingBirthdays.forEach((birthday) => {
+    bot.telegram.sendMessage(-1001434326296, `Hey, today is ${birthday.name}'s Birthday! 🎂`)
+  })
+}, 24 * 60 * 60 * 1000)
+
+bot.launch()
